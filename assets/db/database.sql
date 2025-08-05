9e1f30b4a15ff9c8951e4878da83843562377e1f
XMASTA007 <92289753+XMASTA007@users.noreply.github.com>
2025-08-05 18:10:49 +0200

    sql updated

--- assets/db/database.sql
+++ assets/db/database.sql
@@ -3,8 +3,6 @@ USE contact_form_db;
 
 CREATE TABLE messages (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    email VARCHAR(100) NOT NULL,
     message TEXT NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
