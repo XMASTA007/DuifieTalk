3ccb2a6e5d57de60ef37277a1d1a4db671ecb03e
XMASTA007 <92289753+XMASTA007@users.noreply.github.com>
2025-08-06 16:40:42 +0200

    del

--- assets/php/send_email.php
+++
@@ -1,7 +0,0 @@
-<?php
-$to = 'nathanbeukes33@gmail.com';
-$subject = 'New Contact Form Message';
-$headers = 'From: nathanbeukes33@gmail.com' . "\r\n" .
-    'Reply-To: nathanbeukes33@gmail.com' . "\r\n" .
-    'X-Mailer: PHP/' . phpversion();
-?>
